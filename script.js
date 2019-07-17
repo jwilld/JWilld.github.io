e60ee52624fff14c4bceed9f466da3cbbb96a3ed
Jerry Davidson <willdjerry@gmail.com>
2019-07-17 09:56:07 -0400

    When 'New Word' is clicked, the underlines are removed and the word to guess box input is displayed

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ let wordForm = document.querySelector('.word-input')
 wordSubmit.addEventListener('click',getInput)
 
 
-//take a user inpiut for desired word
+//takes a user's input for desired word
 let guessWord = null
 function getInput(){
     let wordInput = document.querySelector('.text-input').value
     guessWord = Array.from(wordInput)
     wordForm.style.display='none'
-    createSpaces()
+    createUnderLines()
 }
 
-// create elements based on whatever input the user gives for their term
-// need to account for spaces
-let layoutGrid = document.querySelector('.spaces')
-function createSpaces(){
+// creates elements based on whatever input the user gives for their term
+// doesnt account for spaces
+//displays it in a specficic area on the page(grid row/column) I used the css to accomplish this
+
+let underLineContainer = document.querySelector('.under-line')
+function createUnderLines(){
     guessWord.forEach( letter => {
         let hr = document.createElement('hr')
-        layoutGrid.appendChild(hr)
+        underLineContainer.appendChild(hr)
     })
 }
 
+// allow the user to change the word
+let newWord = document.querySelector('.new-word')
 
-//display it in a specficic area on the page(grid row/column)
+newWord.addEventListener('click',makeNewWord)
 
+function makeNewWord(){
+    // removes the hr items
+    let hrList = document.querySelectorAll('.under-line hr')
+    hrList.forEach( item => item.remove())
+    wordForm.style.display = 'block'
+}
